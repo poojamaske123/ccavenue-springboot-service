995f846ff16374068eb66faccc92831afd28ad8c
poojamaske123 <103932430+poojamaske123@users.noreply.github.com>
2023-11-08 12:05:36 +0530

    Feat:changed api path

--- src/main/java/com/himcab/CcAvenue/controller/CcAvenueController.java
+++ src/main/java/com/himcab/CcAvenue/controller/CcAvenueController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
 @Controller
-@RequestMapping("/api/public")
+@RequestMapping("/api/public/ccavenue")
 public class CcAvenueController {
 
     @GetMapping("/hello")
