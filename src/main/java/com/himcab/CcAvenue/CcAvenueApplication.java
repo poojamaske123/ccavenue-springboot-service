4932103c3ee76a5570dfc23fafbee6ec3cf8c3f7
poojamaske123 <103932430+poojamaske123@users.noreply.github.com>
2023-11-08 16:42:54 +0530

    Feat:added annotation

--- src/main/java/com/himcab/CcAvenue/CcAvenueApplication.java
+++ src/main/java/com/himcab/CcAvenue/CcAvenueApplication.java
@@ -2,8 +2,10 @@ package com.himcab.CcAvenue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.himcab.CcAvenue.controller"})
 public class CcAvenueApplication {
 
 	public static void main(String[] args) {
