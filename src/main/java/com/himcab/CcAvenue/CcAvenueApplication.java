63b21f3c553908339b0a189c35e3bdbcdfe8c523
poojamaske123 <103932430+poojamaske123@users.noreply.github.com>
2023-11-20 13:33:56 +0530

    Feat:War packaging added

--- src/main/java/com/himcab/CcAvenue/CcAvenueApplication.java
+++ src/main/java/com/himcab/CcAvenue/CcAvenueApplication.java
@@ -2,11 +2,12 @@ package com.himcab.CcAvenue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"com.himcab.CcAvenue.controller"})
-public class CcAvenueApplication {
+public class CcAvenueApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CcAvenueApplication.class, args);
